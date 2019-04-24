e5a33365e03ef7fae41bb73697fe7762f3be8458
John Nolette <jnolette@transparent.com>
2019-04-24 17:10:41 -0400

    Adding invokation for other scripts

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,3 +1,8 @@
 # install chocolately / choco cli
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+$BaseDir = Split-Path $MyInvocation.InvocationName
+$ScriptPath = "$BaseDir\scripts"
+& "$ScriptPath\common.ps1"
+& "$ScriptPath\development.ps1"
